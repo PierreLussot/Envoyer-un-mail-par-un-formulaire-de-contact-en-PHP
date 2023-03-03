6d07e181667796a32dbd3f458c78c7b6d142a03d
Pierre <pierrelidem@gmail.com>
2023-03-03 11:59:51 +0100

    envoi email

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
+<?php
+if (isset($_POST['send'])) {
+    extract($_POST);
+    if (
+        isset($username) && isset($email) &&
+        isset($phone) && isset($message) &&
+        $username != "" && $email != "" &&
+        $phone != "" && $message != ""
+    ) {
+        //le destinataire (votre email)
+        $to = "testcodeidconnect@gmail.com";
+        $subject = "Vous avez recu un message de : " . $email;
+
+        $message = "
+        <p>Vous avez recu un message de <strong> " . $email . "</strong> </p>
+        <p><strong>Nom : </strong> " . $username . "</p>
+        <p><strong>Telephone : </strong> " . $phone . "</p>
+        <p><strong>Message : </strong> " . $message . "</p>
+        ";
+
+        // Always set content-type when sending HTML email
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+        // More headers
+        $headers .= 'From: <' . $email . '>' . "\r\n";
+
+        //envoi du mail
+        mail($to, $subject, $message, $headers);
+        //verificationnd l'envoie
+
+        if ($send) {
+            $_SESSION['succes_message'] = "message envoyé";
+        } else {
+            $info = "message non envoyé";
+        }
+    } else {
+        $info = "Veuillez remplir tous les champs";
+    }
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,14 +53,33 @@
 </head>
 
 <body>
-    <p class="request_message">
-        message envoyer !
-    </p>
+
+    <?php
+    //affichage message d'erreur
+    if (isset($info)) {
+    ?>
+        <p class="request_message" style="color:red">
+            <?= $info ?>
+        </p>
+    <?php
+    }
+    ?>
+
+    <?php
+    //afficher le message de succes
+    if (isset($_SESSION['succes_message'])) {
+    ?>
+        <p class="request_message" style="color:green">
+            <?= $_SESSION['succes_message']  ?>
+        </p>
+    <?php
+    }
+    ?>
     <form action="" method="POST">
         <label for="">Nom</label>
         <input type="text" name="username">
         <label for="">Email</label>
-        <input type="email" name="name">
+        <input type="email" name="email">
         <label for="">Téléphone</label>
         <input type="number" name="phone">
         <label for="">Message</label>
